4eb8822addc2d59b07deaabdaf504c7b7d5e6a9e
M4lanie2 <ekwuemejohn@gmail.com>
2024-12-18 19:15:12 +0000

    Add files via upload

---
+++ practise 1.py
@@ -0,0 +1,15 @@
+Studentname='kaira'
+print(Studentname)
+print("youre name is",Studentname)
+schoolname="springfing field   juniors"
+age=11
+grade=6
+HEIGHT=5.4
+COUNTRY="UNITEDKINDOM"
+print("my school name is ",schoolname)
+print("youre age is butter chicken",age)
+print("youre grade iss ",grade)
+FOOD=input("enter yoire fav food")
+print(FOOD)
+
+
